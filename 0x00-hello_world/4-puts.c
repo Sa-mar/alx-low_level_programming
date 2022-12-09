014bb351e0aaec3363abbb787f6b1532030f876c
Aliyu Garba Muhammad <aliyugarba85@yahoo.com>
2022-12-08 19:39:29 -0800

    c q5 second modification

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
-/*Print Text using puts*/
+
+/*
+ * main - function can be called witout any parameter
+ *
+ * Purpose - Print Text using puts
+ */
 int main(void)
 	/*Print text using puts*/
 {
-   char printing[]="\"Programming is like building a multilingual puzzle";
-   puts(printing);
+   puts("\"Programming is like building a multilingual puzzle");
    return 0;
 }
