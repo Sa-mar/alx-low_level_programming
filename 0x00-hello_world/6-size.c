9bf8623acc10ce0b978b64c03ed28dee1664f145
Aliyu Garba Muhammad <aliyugarba85@yahoo.com>
2022-12-12 20:38:32 -0800

    q7 m1

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,1 +1,25 @@
 #include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Description: Print the sizes of types
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+	int i;
+	long l;
+	long long ll;
+	float f;
+
+	printf("Size of a char: %d byte(s)\n", sizeof(c));
+	printf("Size of a int: %d byte(s)\n", sizeof(i));
+	printf("Size of a long int: %d byte(s)\n", sizeof(l));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(ll));
+	printf("Size of a float: %d byte(s)\n", sizeof(f));
+
+	return (0);
+}
