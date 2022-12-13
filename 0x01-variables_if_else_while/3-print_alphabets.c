8196ea50c2c111bd67ec4c9b5e1d816b30a911f2
Aliyu Garba Muhammad <aliyugarba85@yahoo.com>
2022-12-12 17:30:28 -0800

    q4

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Description: print lower and upper case letters
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 97; i<=122; i++)
+	{
+		putchar(i);
+	}
+	for (i = 65, i<=90; i++)
+	{
+		putchar(i);
+	}
+	putchar(10);
+	return (0);
+}
