52055b1fbfbf7a486c3ae175e0b0cbff9cb9dd03
Aliyu Garba Muhammad <aliyugarba85@yahoo.com>
2022-12-12 20:20:11 -0800

    q5 m 1

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -8,8 +8,8 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-	/*Print text using puts*/
+/*Print text using puts*/
 {
-   puts("\"Programming is like building a multilingual puzzle");
-   return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
