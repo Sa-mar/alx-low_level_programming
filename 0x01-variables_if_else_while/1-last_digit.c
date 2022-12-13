b5ec75969c66cc13942d28eb47d07fba92befa62
Aliyu Garba Muhammad <aliyugarba85@yahoo.com>
2022-12-12 16:26:09 -0800

    if else q2 modification 4

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -30,7 +30,7 @@ int main(void)
 		{
 			if (last_digit < 6 && last_digit !=0)
 			{
-				printf("Last digit of %d is %d and is less than 8 and not 0\n", n, last_digit);
+				printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
 			}
 		}
 	}
