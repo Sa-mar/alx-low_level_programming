ec90d66768c481f046a12c9e8d58d1968f27975d
Aliyu Garba Muhammad <aliyugarba85@yahoo.com>
2022-12-13 18:36:09 -0800

    print alphabtes x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Entry point
+ *
+ * Description: print lowercase 10 times
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	int a = 0;
+
+	while (a < 10)
+	{
+		int i;
+
+		for (i = 97; i <= 122; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('n\');
+	}
+}
