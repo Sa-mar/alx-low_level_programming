c83133b949b6908dc452a58c3acf348ac2881e4d
Aliyu Garba Muhammad <aliyugarba85@yahoo.com>
2022-12-13 19:43:16 -0800

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_last_digit - Entry Point
+ * @i: The digit
+ *
+ * Description: To get last digit of a number
+ *
+ * Return: The last number
+ */
+int print_last_digit(int i)
+{
+	int last_digit;
+
+	last_digit = i % 10;
+	if (i < 10)
+	{
+		i *= -1;
+	}
+	_putchar(last_digit + '0');
+	return (last_digit);
+}
