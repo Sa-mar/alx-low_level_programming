4472ce9ab3ebd61d5052ac7c4098d1f7de82b794
Aliyu Garba Muhammad <aliyugarba85@yahoo.com>
2022-12-13 18:11:03 -0800

    making 7th correction to q1

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -12,14 +12,13 @@
  */
 int main(void)
 {
-	_putchar("_");
-	_putchar("p");
-	_putchar("u");
-	_putchar("t");
-	_putchar("c");
-	_putchar("h");
-	_putchar("a");
-	_putchar("r");
-	_putchar("\n");
+	char putchr[] = "_putchar";
+	int i;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(putchr[i]);
+	}
+	_putchar("\n";
 	return (0);
 }
