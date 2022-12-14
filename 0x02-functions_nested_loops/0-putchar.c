745ce80ae8b8772e48ad52ee5a649247fab2d691
Aliyu Garba Muhammad <aliyugarba85@yahoo.com>
2022-12-13 17:30:09 -0800

    Program that prints _putchar, followed by a line

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Description: print a string and a new line
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	putchar("_putchar");
+	putchar(10);
+	return (0);
+}
