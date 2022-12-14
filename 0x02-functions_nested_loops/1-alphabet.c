a52fda7676b02c619c389b7e6881fb81ed67df7a
Aliyu Garba Muhammad <aliyugarba85@yahoo.com>
2022-12-13 18:23:44 -0800

    print alphabets in small letter

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry Point
+ *
+ * Description: print alphabet in lower case
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 97, i<=122, i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+	return (0);
+}
