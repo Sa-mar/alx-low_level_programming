5dc27952555afaa6f10d43d1f3ddd5f62e7bdf28
Aliyu Garba Muhammad <aliyugarba85@yahoo.com>
2022-12-13 19:24:02 -0800

    return absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _abs - Entry Point
+ * @n: The integer
+ *
+ * Description: computes absolute value
+ *
+ * Return: Absolute value
+ */
+int _abs(int i)
+{
+	if (i >= 0)
+	{
+		return (i);
+	}
+	else
+	{
+		return (-i);
+	}
+}
