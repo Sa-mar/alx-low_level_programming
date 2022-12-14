cb7ffaab2668973c98f89537ddd08d9f647d3741
Aliyu Garba Muhammad <aliyugarba85@yahoo.com>
2022-12-13 18:48:28 -0800

    checking if charcter is lower case

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _islower - Entry Point
+ *
+ * Description: checks if alphabets are in lower case
+ *
+ * Return: Always 1 (Success)
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
