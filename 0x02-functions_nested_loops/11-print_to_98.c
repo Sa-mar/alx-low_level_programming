c5b1138adfea5bafe69d1b6314a25a8af0567bb0
Aliyu Garba Muhammad <aliyugarba85@yahoo.com>
2022-12-13 20:20:59 -0800

    print natural numbers

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,32 @@
+#Include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_to_98 - Entry Point
+ * @n: starting point
+ *
+ * Description: count natural numbers
+ *
+ * Return: void
+ */
+void print_to_98(int n)
+{
+	if (n > 98)
+	{
+		for (n = n; n > 98; --n)
+		{
+			_putchar('%d, ', n);
+		}
+		_putchar('%d\n', 98)
+	}
+	else
+	{
+		for (n = n; n < 98; ++n)
+		{
+			_putchar('%d, ', n);
+		}
+		_putchar('%d\n', 98);
+	}
+}
