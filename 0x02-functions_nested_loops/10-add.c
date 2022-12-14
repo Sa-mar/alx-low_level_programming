a03d888b7416a8be6b975bde92e650e4325fd799
Aliyu Garba Muhammad <aliyugarba85@yahoo.com>
2022-12-13 20:09:02 -0800

    sum of two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * add - Entry Point
+ * @x: number 1
+ * @y: number 2
+ *
+ * Description: Add two number
+ *
+ * Return: summation
+ */
+int add(int x, int y)
+{
+	return (x + y);
+}
