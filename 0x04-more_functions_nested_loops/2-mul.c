b0ec5a1749f08662228182a04f9fb25f5833b0be
Aliyu Garba Muhammad <aliyugarba85@yahoo.com>
2022-12-15 17:39:41 -0800

    multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * mul - Entry Point
+ * @x: 1st number
+ * @y: 2nd number
+ *
+ * Description: multiplies 2 integers
+ *
+ * Return: Result of multiplication
+ */
+int mul(int x, int y)
+{
+	return (x * y);
+}
