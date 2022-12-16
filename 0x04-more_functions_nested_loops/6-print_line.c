59488525305e12dbfb755e83669085a54d2f32aa
Aliyu Garba Muhammad <aliyugarba85@yahoo.com>
2022-12-15 18:21:50 -0800

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_line - Entry Point
+ * @n: the distance
+ *
+ * Description: Function that prints a line
+ *
+ * Return: void
+ */
+void print_line(int n)
+{
+	int x;
+	x = 0;
+
+	while (x < n)
+	{
+		_putchar('_');
+		x = x + 1;
+	}
+	-Putchar('\n');
+}
