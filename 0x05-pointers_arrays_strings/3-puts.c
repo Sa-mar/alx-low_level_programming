ee24e941c400f50f74b429715eb4bafa61b2eecc
Aliyu Garba Muhammad <aliyugarba85@yahoo.com>
2022-12-22 14:38:34 -0800

    s function that prints a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts - Entry Point
+ * @str: The string
+ *
+ * Description: A function that prints a string
+ *
+ * Return: Void
+ */
+void _puts(char *str)
+{
+	while (*str)
+	{
+		_putchar(*str++);
+		*str++;
+	}
+	_putchar(10);
+}
