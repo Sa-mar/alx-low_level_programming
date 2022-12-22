36b91785ebc7f1e1f1e126c09a150c77f1a681e5
Aliyu Garba Muhammad <aliyugarba85@yahoo.com>
2022-12-22 14:22:15 -0800

    function to get length of string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * _strlen - ENtry Point
+ * @x: the string
+ *
+ * Description: Function that returns str length
+ *
+ * Return: The length
+ */
+int _strlen(char *x)
+{
+	int y = 0;
+
+	while (*x[y] != '\0')
+	{
+		y++;
+	}
+/*https://www.programmingsimplified.com/c-program-find-string-length*/
+}
