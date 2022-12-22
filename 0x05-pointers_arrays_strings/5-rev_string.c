20c233e08e3d590f557f925fbe043568eb3ec0b1
Aliyu Garba Muhammad <aliyugarba85@yahoo.com>
2022-12-22 15:49:09 -0800

    reverse a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * rev_string - Entry Point
+ * @x: The string
+ *
+ * Description: Reverse a string in revers
+ *
+ * Return: Void
+ */
+void rev_string(char *x)
+{
+	int a, b, c;
+	char result;
+
+	c = 0;
+	for (a = 0; x[a] != '\0'; ++a)
+	{
+		c++;
+	}
+	b = 0;
+	for (a = c - 1; a >= 0; --a)
+	{
+		result[++b] = x[a];
+	}
+}
