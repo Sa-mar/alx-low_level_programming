f4d4dca910c64eb179e33463f4b44517c572a278
Aliyu Garba Muhammad <aliyugarba85@yahoo.com>
2022-12-25 12:46:18 -0800

    printing quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Description: printing to standout error
+ *
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+	fprintf(stdout, "%s", "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+
+	return (1);
+}
