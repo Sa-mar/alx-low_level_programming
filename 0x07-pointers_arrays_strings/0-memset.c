785d735298e6765d5f5ef546179726729d5c1e91
Aliyu Garba Muhammad <aliyugarba85@yahoo.com>
2023-01-02 15:04:15 -0800

    1st commit to filling memory with a constant byte

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * _memset - Entry Point
+ * @s: Pointer to memory
+ * @b: constant value
+ * @n: integer value
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int a;
+
+	for (a = 0; a < n; ++a)
+	{
+		s[a] = b;
+	}
+	return (s);
+}
