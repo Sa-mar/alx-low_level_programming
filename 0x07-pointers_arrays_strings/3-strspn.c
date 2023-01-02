1fb8e3ed9f4c034b0239b2b4035afe4009a68036
Aliyu Garba Muhammad <aliyugarba85@yahoo.com>
2023-01-02 15:54:09 -0800

    first commit to get length of prefix

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * _strspn - Entry point
+ * @s: The initial segment
+ * @accept: the only bytes
+ *
+ * Description: function that gets lengh of a prefix
+ *
+ * Return: number of bytes
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int a = 0;
+	int b;
+
+	while (s[a])
+	{
+		b = 0;
+		while (accept[b])
+		{
+			if (s[a] == accept[b])
+			{
+				++b;
+				break;
+			}
+			if (accept[a + 1] == '\0')
+			{
+				return (a);
+			}
+		}
+		a++;
+	}
+}
