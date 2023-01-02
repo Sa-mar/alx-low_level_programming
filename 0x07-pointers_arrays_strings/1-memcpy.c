b045c6ecc30bc1e051e369a5c5e414207ffb4161
Aliyu Garba Muhammad <aliyugarba85@yahoo.com>
2023-01-02 15:17:54 -0800

    first commit to function that copies to memory area

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * _memcpy - Entry Point
+ * @dest: memory area
+ * @src: memory area
+ * @n: integer
+ *
+ * Description: a function that copies to memory area
+ *
+ * Return: a pointer
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int a;
+
+	for (a = 0; a < n; ++src)
+	{
+		dest[a] = *src;
+	}
+	return (dest);
+}
