d56edc45e27a53df4a8812f38c3972696294e44d
Aliyu Garba Muhammad <aliyugarba85@yahoo.com>
2023-01-02 15:33:40 -0800

    first commit to function that locate a praticular character

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * _strchr - Entry Point
+ * @s: string
+ * @c: character
+ *
+ * Description: function that locates charcter in a string
+ *
+ * Return: returns a pointer
+ */
+char *_strchr(char *s, char c)
+{
+	int a = 0;
+
+	while (s[a] >= '\0')
+	{
+		if (s[a] == c)
+		{
+			return (s);
+		}
+		a++;
+	}
+	return ('\0');
+}
