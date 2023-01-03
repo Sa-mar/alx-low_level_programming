501ea90ac993d4e89bcd5b42787239063f8f942e
Aliyu Garba Muhammad <aliyugarba85@yahoo.com>
2023-01-02 16:07:33 -0800

    first commit to searching for a string

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strbrk - Entry Point
+ * @s: string
+ * @accept: to match
+ *
+ * Description: a function to search string
+ *
+ * Return: a pointer
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int a;
+
+	while (*s)
+	{
+		a = 0;
+		while (accept[a])
+		{
+			if (*s == accept[a])
+			{
+				return (s);
+			}
+			a++;
+		}
+		s++;
+	}
+	return ('\0');
+}
