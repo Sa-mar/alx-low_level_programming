d11914e19f56812849bcb8e07ea929d1c66d46e3
Aliyu Garba Muhammad <aliyugarba85@yahoo.com>
2023-01-04 17:46:48 -0800

    a function that computes x to power y

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _pow_recursion - The power rec function
+ * @x: the value
+ * @y: the power value
+ *
+ * Description: A function that computes power of a number
+ *
+ * Return : the value or -1 if power < 0
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	else if (y == 1)
+	{
+		return (x);
+	}
+	else
+	{
+		return (_pow_recursion(x, y - 1) * x);
+	}
+}
