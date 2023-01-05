13980bbc1e04e6ad4a01f9fb37bbf2c73bfb90a8
Aliyu Garba Muhammad <aliyugarba85@yahoo.com>
2023-01-04 16:38:25 -0800

    A function that prints string follwed by a new line

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -13,13 +13,13 @@
  */
 void _puts_recursion(char *s)
 {
-	if (*str == '\0')
+	if (*s == '\0')
 	{
 		_putchar('\n');
 	}
 	else
 	{
-		_putchar(*str);
+		_putchar(*);
 		_puts_recursion(s + 1);
 	}
 }
