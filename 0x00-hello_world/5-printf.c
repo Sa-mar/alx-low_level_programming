20f52db7655fcaed276bb6faf04ef6b556271a52
Aliyu Garba Muhammad <aliyugarba85@yahoo.com>
2023-03-16 16:39:57 -0700

    using printf to print

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: using printf to print text
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
