3dd8a34c3b536513365d630bd23e082797c173c2
Aliyu Garba Muhammad <aliyugarba85@yahoo.com>
2023-03-16 17:13:22 -0700

    printing to std error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Description: other way of print to std error
+ *
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
