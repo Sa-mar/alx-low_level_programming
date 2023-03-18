3a1932535dba46159cd5610d1083432e32368497
Aliyu Garba Muhammad <aliyugarba85@yahoo.com>
2023-03-17 18:32:59 -0700

    using putchar to print from 0 to 9

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Description: Print numbers from 0 to 9
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a = 48;
+
+	while (a <= 57)
+	{
+		putchar(a);
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
