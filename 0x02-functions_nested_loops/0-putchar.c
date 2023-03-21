6a2dd4a4f61346d19c5de377f9e431c1d8bf7b01
Aliyu Garba Muhammad <aliyugarba85@yahoo.com>
2023-03-21 15:15:16 -0700

    pushing main.h and _putchar function

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _putchar - the function
+ *
+ * Description: To print output
+ *
+ * Return: Always 0 (Success)
+ */
+int _putchar(a)
+{
+	a = "_putchar";
+	
+	putchar(a);
+	putchar('\n');
+	return (0);
+}
