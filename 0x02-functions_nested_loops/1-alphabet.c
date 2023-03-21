02d83b5cf80cd3f5a89a8ca3c3d61e6cd4e463a4
Aliyu Garba Muhammad <aliyugarba85@yahoo.com>
2023-03-21 16:06:14 -0700

    a function to print lower case

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet - The function
+ *
+ * Description: Fundtion to print alphabet
+ *
+ * Return: A void function
+ */
+void print_alphabet(void)
+{
+	int a;
+
+	for (a = 97; a <= 122; a++)
+	{
+		_putchar(a);
+		_putchar('\n');
+	}
+}
