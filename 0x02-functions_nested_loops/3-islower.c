8617fdb263cd9079c380aaedc00d36e58d346c98
Aliyu Garba Muhammad <aliyugarba85@yahoo.com>
2023-03-21 16:24:33 -0700

    checking for lower case

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _islower - The function
+ *
+ * Description: A function that check lower case
+ *
+ * Return: 1 for lower and 0 otherwise
+ */
+int _islower(int c)
+{
+	if (c >= 97 || c <= 122)
+	{
+		_putchar(1);
+	}
+	else
+	{
+		_putchar('\0');
+	}
+}
