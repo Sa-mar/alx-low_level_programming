6d7d56b45ff0d28cf948122b84d178367834267c
Aliyu Garba Muhammad <aliyugarba85@yahoo.com>
2023-03-21 17:24:59 -0700

    function to get absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * int _abs - The prototype
+ *
+ * Description: a function that computes the absolute value of an integer
+ *
+ * Return: Always o
+ */
+int _abs(int)
+{
+	int n;
+
+	if (n == 0)
+	{
+		_putchar(n);
+	}
+	else if (n > 0)
+	{
+		_putchar(n);
+	}
+	else
+	{
+		_putchar((n * -1) + '0');
+	}
+	return (0)
+}
