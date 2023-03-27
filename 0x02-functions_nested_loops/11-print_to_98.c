075fc36513917af7e5f5e44096685d1c6aba0284
Aliyu Garba Muhammad <aliyugarba85@yahoo.com>
2023-03-26 17:44:18 -0700

    printing to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_to_98 - The prototype
+ * @n: the integer
+ *
+ * Description: A function that prints to 98
+ *
+ * Return: int
+ */
+void print_to_98(int n)
+{
+	if (n = 98);
+	{
+		_putchar(n);
+	}
+	else if (n < 98)
+	{
+		for (n = n; n <= 98; n++)
+		{
+			_putchar((n / 10) + '0');
+			_putchar((n % 10) + '0');
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+	else if (n > 98)
+	{
+		for (n = n; n>= 98; n--)
+		{
+			_putchar((n / 10) + '0');
+			_putchar((n % 10) + '0');
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+}
