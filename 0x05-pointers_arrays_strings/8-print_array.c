37f6bf2748fb862e5c0379a41b36e2e572982100
Aliyu Garba Muhammad <aliyugarba85@yahoo.com>
2023-03-28 16:17:00 -0700

    print n elements of array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -18,12 +18,12 @@ void print_array(int *a, int n)
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", a[i]);
-		if (i == n - 1)
+		/*if (i == n - 1)
 		{
 			continue;
-		}
+		}*/
 		printf(",");
 		printf(" ");
 	}
-	_putchar('\n');
+	printf("\n");
 }
