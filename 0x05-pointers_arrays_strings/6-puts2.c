1c6ffc59a0b3a3853ff79a679a7512ec016d7aab
Aliyu Garba Muhammad <aliyugarba85@yahoo.com>
2023-03-28 13:38:38 -0700

    function that prints every other char

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts2 - The function
+ * @str: The string pointer
+ *
+ * Description: A function that prints every oda char
+ *
+ * Return: void
+ */
+void _puts2(char *str)
+{
+	int st;
+
+	for (st = 0; str[st] != '\0'; st = st + 2)
+	{
+		_putchar(str[st]);
+	}
+	_putchar('\n');
+}
