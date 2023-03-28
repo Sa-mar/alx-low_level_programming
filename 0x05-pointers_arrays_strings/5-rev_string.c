74ca15c08e7d70efdddd5a611096606fbcab01dd
Aliyu Garba Muhammad <aliyugarba85@yahoo.com>
2023-03-28 12:57:03 -0700

    function that reverses string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -14,14 +14,13 @@ void rev_string(char *s)
 {
 	int st;
 	int count;
+	int tmpstr;
 
 	for (count = 0; s[count] != '\0'; count++)
 	{
 	}
-	_putchar(s[count]);
 	for (st = count - 1; st >= 0; st--)
 	{
-		_putchar(s[st]);
 	}
-	_putchar('\n');
+	tmpstr = s[st];
 }
