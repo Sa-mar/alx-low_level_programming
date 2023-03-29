977b5d71e2f8269edfcfe6a46e77298502c36805
Aliyu Garba Muhammad <aliyugarba85@yahoo.com>
2023-03-28 17:43:35 -0700

    copy string from src to dest

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * *_strcpy - The function
+ * @dest: destination pointer
+ * @src: source pointer
+ *
+ * Description: a function to copy string 4rm src 2 dest
+ *
+ * Return: dest
+ */
+char *strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != ''\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+	return (dest);
+}
