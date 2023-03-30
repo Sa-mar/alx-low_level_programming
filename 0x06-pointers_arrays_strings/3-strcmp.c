1741bd4717ea98d75d7ea7330c2e8d482ecd73d4
Aliyu Garba Muhammad <aliyugarba85@yahoo.com>
2023-03-30 16:52:30 -0700

    compare two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -13,23 +13,14 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i, stat;
+	int i;
 
-	stat = 0;
 	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
 	{
 		if (s1[i] != s2[i])
 		{
-			stat = 1;
 			break;
 		}
 	}
-	if (stat == 0)
-	{
-		return (0);
-	}
-	else
-	{
-		return (1);
-	}
+	return (s1[i] - s2[i]);
 }
