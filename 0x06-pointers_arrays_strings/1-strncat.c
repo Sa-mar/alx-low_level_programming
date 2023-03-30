849ffefdfbd478412590a092e06a0471061aa903
Aliyu Garba Muhammad <aliyugarba85@yahoo.com>
2023-03-30 07:42:48 -0700

    concat but with conditions

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * *_strncat - The function
+ * @dest: dest string
+ * @src: Source string
+ * @n: the integer
+ *
+ * Description: A function that concat 2 strings
+ *
+ * Returns: dest string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, len1;
+
+	for (len1 = 0; dest[len1] != '\0'; len1++)
+	{
+	}
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[len1 + 1] = src[i];
+	}
+	return (dest);
+}
