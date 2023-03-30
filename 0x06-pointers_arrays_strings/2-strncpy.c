1df4b7260220cd12b46006a71930522a1a0d76fe
Aliyu Garba Muhammad <aliyugarba85@yahoo.com>
2023-03-30 15:28:50 -0700

    own strncpy function

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * *_strncpy - the function
+ * @dest: Dest string
+ * @src: source string
+ * @n: the int
+ *
+ * Description: a function that copies string
+ *
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
