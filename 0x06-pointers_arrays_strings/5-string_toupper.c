0802851cc3f25a8b827cabd166dcb1bb91e5ea92
Aliyu Garba Muhammad <aliyugarba85@yahoo.com>
2023-03-30 19:58:19 -0700

    convert lower to upper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * *string_toupper - The function
+ * @a: the string
+ *
+ * Description: A function that convert 4rm l to u
+ *
+ * Return: the upper
+ */
+char *string_toupper(char *a)
+{
+	int i;
+
+	for (i = 0; a[i] != '\0'; i++)
+	{
+		if (a[i] >= 97 && a[i] <= 122)
+		{
+			a[i] = a[i] - 32;
+		}
+	}
+	return (a);
