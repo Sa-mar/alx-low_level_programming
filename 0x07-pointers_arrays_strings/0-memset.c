c01e44e99e590dc897d7ce2541ae4a497b3ccaa6
Aliyu Garba Muhammad <aliyugarba85@yahoo.com>
2023-04-03 17:26:10 -0700

    own implementation of memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -3,7 +3,7 @@
 #include "main.h"
 
 /**
- * *memset - The function
+ * *_memset - The function
  * @s: The buffer
  * @b: The character
  * @n: the number of bytes
@@ -14,7 +14,7 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	int i;
+	unsigned int i;
 
 	for (i = 0; i < n; i++)
 	{
