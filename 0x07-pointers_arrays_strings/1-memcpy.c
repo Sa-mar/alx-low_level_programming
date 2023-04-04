7fbd6c6e1d7d347f810eb15fe9120e9bf9cace25
Aliyu Garba Muhammad <aliyugarba85@yahoo.com>
2023-04-03 17:33:32 -0700

    own implementation of memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,24 @@
+#include <stiolib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * *_memcpy - The function
+ * @dest: Destination address
+ * @src: source address
+ * @n: The number of times/bytes
+ *
+ * Description: A function that copies memoery area
+ *
+ * Return: Pointer to dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
