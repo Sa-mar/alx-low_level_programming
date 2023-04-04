ec145a6938083550d41a5bf058b49989c6484f0f
Aliyu Garba Muhammad <aliyugarba85@yahoo.com>
2023-04-03 20:29:38 -0700

    own implementation of strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,49 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strspn - The function
+ * @s: string to check
+ * @accept: string to compare with
+ *
+ * Description: a func to gets the leng of a prefix subs
+ *
+ * Return: the length
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, j, len1, len2, status, lenght;
+
+	lenght = 0;
+	status = 0;
+
+	for (len1 = 0; s[len1] != '\0'; len1++)
+	{
+	}
+
+	for (len2 = 0; accept[len2] != '\0'; len2++)
+	{
+	}
+
+	for (i = 0; i < len1, i++)
+	{
+		for (j = 0; j < len2; j++)
+		{
+			if (accept[j] == s[i])
+			{
+				status = 1;
+				break;
+			}
+		}
+		if (status == 1)
+		{
+			lenght++;
+		}
+		else
+		{
+			break;
+		}
+	}
+	return (lenght)
+}
