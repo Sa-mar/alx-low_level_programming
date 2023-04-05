715b679953021279de7e84f928c3eb45f8086641
Aliyu Garba Muhammad <aliyugarba85@yahoo.com>
2023-04-05 16:36:59 -0700

    factorial using recursion

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -14,10 +14,15 @@ int factorial(int n)
 {
 	if (n < 1)
 	{
-		return (0);
+		return (-1);
+	}
+
+	else if (n <= 1 && n >= 0)
+	{
+		return (1);
 	}
 	else
 	{
-		return (n * factorial(n - 1));
+		return (n * factorial(n -1));
 	}
 }
