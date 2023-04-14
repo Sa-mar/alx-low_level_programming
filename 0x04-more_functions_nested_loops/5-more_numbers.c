85ba611f4f1022e446a47861cb64bad0a7ff1c6e
Aliyu Garba Muhammad <aliyugarba85@yahoo.com>
2023-04-13 17:59:31 -0700

    print from 0 - 14

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * more_numbers - The function
+ *
+ * Description: print from 0 - 14 x 10
+ *
+ * Return: Void
+ */
+void more_numbers(void)
+{
+	int a, b;
+
+	for (a = 0; a < 10; a++)
+	{
+		for (b = '0'; b <= '14'; b++)
+		{
+			_putchar(b);
+		}
+		_putchar('\n');
+	}
+}
