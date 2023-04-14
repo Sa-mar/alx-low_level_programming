0cbd004341f7824ac7a3303c68c7e566ad479ebb
Aliyu Garba Muhammad <aliyugarba85@yahoo.com>
2023-04-13 17:21:51 -0700

    print some and skip some numbers from 0-9

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_most_numbers - The function
+ *
+ * Description: print some and skip some numbers
+ *
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		if (i == 50 || i == 52)
+		{
+			break;
+		}
+		else
+		{
+			_putchar(i);
+		}
+	}
+	_putchar('\n');
+}
