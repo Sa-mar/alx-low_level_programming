a1179cd5af972879e738fc229aef2c9e75cc9aba
Aliyu Garba Muhammad <aliyugarba85@yahoo.com>
2023-04-17 15:11:40 -0700

    print name of a file

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -2,15 +2,13 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- * @argc: the number of argument
- * @argv: The array that hold the argument
+ * main - Entry pointt
  *
  * Description: prog that prints its name,followed by a new line
  *
  * Return: Always 0 (Sucess)
  */
-int main(__attribute__((unused)) int argc, char *argv[])
+int main(void)
 {
 	printf("%s\n", argv[0]);
 	return (0);
