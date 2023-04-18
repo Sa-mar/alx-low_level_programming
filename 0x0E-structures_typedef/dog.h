d74203221d21b75bb193ac42bb3a09bceca41635
Aliyu Garba Muhammad <aliyugarba85@yahoo.com>
2023-04-17 17:35:49 -0700

    information of dogs

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+/**
+ * struct dog - The Structure
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Description: A structure with info on dogs
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
